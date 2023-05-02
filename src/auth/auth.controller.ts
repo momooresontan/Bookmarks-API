f40f4963f9d72dc5a567b9f25843dec640334ea2
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-03 00:37:00 +0100

    auth dto created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,17 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) {
+    console.log({
+      dto,
+    });
     return this.authService.signup();
   }
 
