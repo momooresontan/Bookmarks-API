c9d3883ab75cc6dda13d982bc4b6f05d7e6de374
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-02 23:45:25 +0100

    Prisma module modified(Prisma service exports)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   signup() {
     return { msg: 'I have signed up' };
   }
