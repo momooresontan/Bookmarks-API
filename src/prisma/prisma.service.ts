036face91be7800cb6bddee5d1dbdb844bd6db5c
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-02 23:35:24 +0100

    Prisma service modified(Prisma client extended)

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:123@localhost:5434/nest?schema=public',
+        },
+      },
+    });
+  }
+}
