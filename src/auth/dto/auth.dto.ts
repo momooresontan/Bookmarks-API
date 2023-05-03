557d576d6a188a8346931f73a162bc91c268727a
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-03 01:14:13 +0100

    global and other pipes used

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,9 +1,13 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+/* eslint-disable prettier/prettier */
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 /* eslint-disable prettier/prettier */
 export class AuthDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
