1da7f19d445432b25c5565cd3a57c8b06bf5eacd
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-03 03:48:23 +0100

    sign up service in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -12,17 +13,25 @@ export class AuthService {
     // Generate the password hash
     const hash = await argon.hash(dto.password);
     //Save the new user in the db
-    const user = await this.prismaService.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    delete user.hash;
-
-    // Return the saved user
-    return user;
+      delete user.hash;
+      // Return the saved user
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Credientials taken');
+        }
+      }
+      throw err;
+    }
   }
 
   login() {
