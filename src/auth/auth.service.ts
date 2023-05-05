82515814e4d76556c6c596ebe46aa03a863119d2
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-05 01:14:21 +0100

    jwt strategy created for authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -57,15 +57,22 @@ export class AuthService {
     return this.signToken(user.id, user.email);
   }
 
-  signToken(userId: number, email: string): Promise<string> {
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
     const payload = {
       sub: userId,
       email,
     };
     const secret = this.config.get('JWT_SECRET');
-    return this.jwt.signAsync(payload, {
+    const token = await this.jwt.signAsync(payload, {
       expiresIn: '15m',
       secret,
     });
+
+    return {
+      access_token: token,
+    };
   }
 }
