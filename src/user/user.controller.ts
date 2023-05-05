e3ce991f45079310c9c8b3e1abd982fc363be926
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-05 13:22:06 +0100

    guard file created

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,12 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from '../auth/guard';
 
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request): string {
-    console.log({
-      user: req.user,
-    });
-    return 'User info';
+  getMe(@Req() req: Request) {
+    return req.user;
   }
 }
