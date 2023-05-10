0356d74aab94d5481e549d640ae0544d52bbcd26
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-10 01:01:10 +0100

    crud operation in bookmark controller initiated

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from '.././auth/guard';
+import { GetUser } from '.././auth/decorator';
+import { BookmarkService } from './bookmark.service';
 
-@Controller('bookmark')
-export class BookmarkController {}
+@UseGuards(JwtGuard)
+@Controller('bookmarks')
+export class BookmarkController {
+  constructor(private readonly bookmarkService: BookmarkService) {}
+  @Get()
+  getBookmarks(@GetUser() userId: number) {}
+
+  @Get()
+  getBookmarkById(@GetUser() userId: number) {}
+
+  @Post()
+  createBookmark(@GetUser() userId: number) {}
+
+  @Patch()
+  updateBookmark(@GetUser() userId: number) {}
+
+  @Delete()
+  deleteBookmarks(@GetUser() userId: number) {}
+}
